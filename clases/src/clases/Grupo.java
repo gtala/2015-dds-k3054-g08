e90515f527876875ef64460bf25da7bdcd7c4434
dandino <daniel.a.andino@gmail.com>
2015-05-23 20:23:53 -0300

    Entrega 2
    Actualización de clases y tests

--- clases/src/clases/Grupo.java
+++ clases/src/clases/Grupo.java
@@ -1,5 +1,17 @@
 package clases;
 
-public class Grupo {
+import java.util.List;
+import java.util.LinkedList;
 
+public class Grupo {
+	List <Usuario> usuarios;
+	
+	public List<Usuario> getUsuarios(){
+		return usuarios;
+	}
+	
+	public LinkedList <Receta> listadoRecetas(){
+		LinkedList <Receta> unListado = new LinkedList <Receta> ();
+		return unListado;
+	}
 }
